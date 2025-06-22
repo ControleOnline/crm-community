8073073a0fc622d93e795cfdfd88ee31f010f78a
Luiz Kim <luiz.kim@controleonline.com>
2025-06-21 21:21:27 -0300

    Add Client

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,18 +1,18 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import loginRoutes from '@controleonline/ui-login/src/react/router/routes';
-import shopRoutes from '@controleonline/ui-shop/src/react/router/routes';
-import ordersRoutes from '@controleonline/ui-orders/src/react/router/routes';
+import crmRoutes from '@controleonline/ui-crm/src/react/router/routes';
 import commonRoutes from '@controleonline/ui-common/src/react/router/routes';
 import peopleRoutes from '@controleonline/ui-people/src/react/router/routes';
+import customersRoutes from '@controleonline/ui-customers/src/react/router/routes';
 
 const Stack = createNativeStackNavigator();
 
 const allRoutes = [
   ...loginRoutes,
-  ...shopRoutes,
-  ...ordersRoutes,
+  ...crmRoutes,
   ...commonRoutes,
   ...peopleRoutes,
+  ...customersRoutes,
 ];
 
 export default function Routes() {
